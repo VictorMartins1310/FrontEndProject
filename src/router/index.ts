b31c9dc82ad9fc622e7fb78cc26fec4f52260f5e
Victor Martins <victor.martins.1310@hotmail.com>
2025-08-26 14:28:09 +0200

    Router bug correction. Before it was a warning with toDoListID prop in the browser, it was passing as String instead a Number

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import ToDoList from '@/views/ToDoList.vue'
+import RegisterView from '@/views/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,7 +23,10 @@ const router = createRouter({
       path: '/:toDotype/:toDoListID',
       name: 'todolist',
       component: ToDoList,
-      props: true,
+      props: route => ({
+        toDotype: route.params.toDotype as string,
+        toDoListID: Number(route.params.toDoListID)
+      })
     },
     {
       path: '/register',
