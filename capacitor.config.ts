19ef449581aa6f1f33144ab022e87eea8086c07b
Victor Martins <victor.martins.1310@hotmail.com>
2026-02-12 01:44:49 +0100

    Option for Exporting to Android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.todoApp.app',
+  appName: 'todoApp',
+  webDir: '../BackEndProject/src/main/resources/static'
+};
+
+export default config;
