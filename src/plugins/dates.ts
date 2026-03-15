5150d2ef81b9761fc67376b98bfe3f3acb19a304
Victor Martins <Victor.Martins.1310@hotmail.com>
2026-03-15 18:59:09 +0100

    Added functions for Calendar, like navigation per day, week, month Functions that checks if the date is in the week, month

--- src/plugins/dates.ts
+++ src/plugins/dates.ts
@@ -1,33 +1,40 @@
 export function getFirstDayWeek(selectedDay: Date): Date {
     const dayOfWeek = selectedDay.getDay();
     return new Date(new Date().setDate(selectedDay.getDate() - dayOfWeek));
-}
+};
 
 export function getLastDayWeek(selectedDay: Date): Date {
     const day1 = getFirstDayWeek(selectedDay);
     return new Date(new Date().setDate(day1.getDate() + 7));
-}
+};
+
+
+export function getFirstDayMonth(selectedDay: Date): Date {
+    let
+        year = selectedDay.getFullYear(),
+        month = selectedDay.getMonth(),
+        day = 1;
+    return new Date(year, month, day);
+};
+
+export function getLastDayMonth(selectedDay: Date): Date {
+    const firstDay: Date = getFirstDayMonth(selectedDay);
+    let lastDay: Date = new Date(new Date().setDate(firstDay.getDate() + 31));
+    while(lastDay.getDate() <= 9 && lastDay.getDate()>= 1){
+        lastDay = dayBefore(lastDay);
+    }
+     
+    return lastDay;
+};
 
 /**
  * 
  * @returns Monday, Sunday
  */
-export function thisWeek(){
-    const 
-        today: Date = new Date();
-        // dayOfWeek = today.getDay(),
-
-    //     day1: Date = new Date(new Date().setDate(today.getDate() - dayOfWeek)),
-    //     day7: Date = new Date(new Date().setDate(day1.getDate() + 7));
-
-    // return {
-    //     "day1": day1,
-    //     "day7": day7
-    // };
-
+export function getFirstLastDayWeek(day: Date){
     return {
-        "day1": getFirstDayWeek(today),
-        "day7": getLastDayWeek(today)
+        "day1": getFirstDayWeek(day),
+        "day7": getLastDayWeek(day)
     }
 };
 
@@ -50,7 +57,7 @@ export function thisMonth(){
 
 export function dayBefore(selectedDay: Date): Date {
     return new Date(selectedDay.getFullYear(), selectedDay.getMonth(), selectedDay.getDate() - 1);
-}
+};
 
 /**
  * Returns new Date (Next Day) as Date
@@ -58,8 +65,48 @@ export function dayBefore(selectedDay: Date): Date {
  * @returns Next Day
  */
 export function nextDay(selectedDay: Date): Date {
-    return new Date(selectedDay.getFullYear(), selectedDay.getMonth(), selectedDay.getDate() + 1);
+    let
+        year = selectedDay.getFullYear(),
+        month = selectedDay.getMonth(),
+        day = selectedDay.getDate() + 1;
+    return new Date(year, month, day);
 }
+
+export function nextWeek(selectedDay: Date): Date {
+    let
+        year = selectedDay.getFullYear(),
+        month = selectedDay.getMonth(),
+        day = selectedDay.getDate() + 7;
+    return new Date(year, month, day);
+}
+
+
+export function weekBefore(selectedDay: Date): Date {
+    let
+        year = selectedDay.getFullYear(),
+        month = selectedDay.getMonth(),
+        day = selectedDay.getDate() - 7;
+    return new Date(year, month, day);
+}
+
+
+export function nextMonth(selectedDay: Date): Date {
+    let
+        year = selectedDay.getFullYear(),
+        month = selectedDay.getMonth() + 1,
+        day = selectedDay.getDate();
+    return new Date(year, month, day);
+}
+
+export function monthBefore(selectedDay: Date): Date {
+    let
+        year = selectedDay.getFullYear(),
+        month = selectedDay.getMonth() - 1,
+        day = selectedDay.getDate();
+    return new Date(year, month, day);
+}
+
+
 /**
  * Verify if Date is in that week
  * @param selectedDay 
@@ -71,3 +118,10 @@ export function isInWeek(selectedDay: Date, date2Proof: Date): boolean {
         return true;
     return false;
 }
+
+export function isInMonth(selectedDay: Date, date2Proof: Date): boolean {
+    if (date2Proof >= getFirstDayMonth(selectedDay) && selectedDay <= getLastDayMonth(selectedDay))
+        return true;
+    return false;
+}
+
