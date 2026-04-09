a991f12b4590463a826da037ebdda11b926ea3cb
Victor Martins <Victor.Martins.1310@hotmail.com>
2026-04-09 03:04:34 +0200

    Removed about component view (missed to remove in router)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import TodoListsView from '@/views/TodoListsView.vue';
-import RegisterView from '@/views/RegisterView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,15 +9,7 @@ const router = createRouter({
       name: 'TodoListsView',
       component: TodoListsView,
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-    // {
+    // { this is just an Example to mantain
     //   path: '/:toDotype/:toDoListID',
     //   name: 'todolist',
     //   component: ToDoList,
@@ -30,7 +21,11 @@ const router = createRouter({
     {
       path: '/register',
       name: 'register',
-      component: RegisterView,
+      // route level code-splitting
+      // this generates a separate chunk (RegisterView.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      // component: () => import('../views/RegisterView.vue'),
+      component: () => import('../views/RegisterView.vue')
     },
   ],
 })
