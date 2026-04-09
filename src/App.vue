3a499822a2565a31e9dfab1894cae61b2073a1ca
Victor Martins <Victor.Martins.1310@hotmail.com>
2026-04-09 02:55:41 +0200

    Clean Up, removed unnecessary css style tags

--- src/App.vue
+++ src/App.vue
@@ -43,69 +43,7 @@ function switchItemForm(value: boolean) {
 
 <style scoped>
 main {
-  /* max-width: 100vw; */
-  min-height: 100vwh;
-}
-
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  min-width: 96vw;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  min-height: calc(100vwh-2rem);
   display: flex;
-  flex-direction: row;
-  top: 2px;
-  left: 2px;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  border-left: 1px solid var(--color-border);
-  height: fit-content;
-  padding: 10px;
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    font-size: 1rem;
-  }
 }
 </style>
