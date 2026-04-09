b73131d57dc143792d402bc3ab0d4c8d4614a3b2
Victor Martins <Victor.Martins.1310@hotmail.com>
2026-04-09 02:53:52 +0200

    Sidebar design and other Design adjustments

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -6,23 +6,47 @@ const auth = useAuthStore();
 defineEmits(['showNewItemForm']);
 </script>
 <template>
-  <div class="SideBar">
-    <p>Expire Time: {{ auth.expireTime  }}</p>
-    <p><button style="padding: 12px;" v-on:click="$emit('showNewItemForm', true)">Add  ➕</button></p>
-    <p v-if="auth.isUserAuthenticated"><RouterLink to="">My Account</RouterLink></p>
-    <button v-if="auth.isUserAuthenticated" v-on:click="auth.logOut()">Log me Out</button>
-  </div>
+  <aside class="wrapper">
+    <div class="SideBar">
+      <!-- <p>Expire Time: {{ auth.expireTime  }}</p> -->
+      <button style="padding: 12px;" v-on:click="$emit('showNewItemForm', true)">Add  ➕</button>
+      <button>
+        <RouterLink to="">My Account</RouterLink>
+      </button>
+      <button v-if="auth.isUserAuthenticated" v-on:click="auth.logOut()">Log me Out</button>
+    </div>
+    </aside>
 </template>
 <style scoped>
+.wrapper {
+  display: flex;
+  align-items: center; /* Reminder: This set item in the middle */
+  position: sticky;
+  min-height: calc(100vh - 2rem); /* Reminder: 2 rem because #App padding have 1rem */
+  padding: 1rem 2rem;
+}
+
 .SideBar {
-  /* position: relative; */
-  top: 40vh;
+  position: flex;
   background-color: #2c2c2c;
   color: white;
+  border-radius: 1rem;
+  max-height: fit-content;
+  padding: 1rem;
+  max-width: 22ch;
+}
 
-  max-width: fit-content;
-  padding: 10px;
-  margin-left: 5px;
-  border-radius: 1em;
+button {
+  width: 100%;
+  background-color: #2c2c2c;
+  border: 0px;
+  border-radius: 0.75rem;
+  color: white;
+  font-weight: bold;
+}
+
+button:hover{
+  background-color: aliceblue;
+  color: black;
 }
 </style>
